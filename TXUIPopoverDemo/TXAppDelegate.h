8a5e4acf5197fb6f5d932c4fa9eeb35d7f35c0e9
tingxuan.qhm <qiuhmm@gamil.com>
2014-01-22 20:47:45 +0800

    Initial Commit

---
+++ TXUIPopoverDemo/TXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TXAppDelegate.h
+//  TXUIPopoverDemo
+//
+//  Created by tingxuan.qhm on 14-1-22.
+//  Copyright (c) 2014年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
